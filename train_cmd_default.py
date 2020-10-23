8494e97a086ce158b5b292da3bb3db542af196d7
SAIL-LAB155 <saillab155@gmail.com>
2020-10-23 18:19:10 +0800

    sparse cmd

--- train_cmd_default.py
+++ train_cmd_default.py
@@ -1,9 +1,9 @@
 #-*-coding:utf-8-*-
 
 cmds = [
-    'python train_sparse.py --cfg pretrained/prev/yolov3-spp-1cls.cfg --batch-size 8 --s 1.5E-3 --epochs 300 -sr '
-    '--LR 1.5E-3 --optimize sgd --weights pretrained/prev/135_608_best.weights --save_interval 10 --multi-scale True '
-    '--img_size 416 --rect True --data data/gray/gray.data  --wdir s1.5E-3_lr1.5E-3'
+    'python train_sparse.py --cfg pretrained/26/yolov3-original-1cls-leaky.cfg --batch-size 8 --s 1E-3 --epochs 400 -sr '
+    '--LR 1E-3 --optimize sgd --weights weights/sparse/1/last.pt --save_interval 10 --multi-scale True '
+    '--img_size 416 --rect True --data data/gray/gray.data  --wdir 1'
 ]
 
 def check_name(cmd):
